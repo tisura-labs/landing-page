31fef867945e76188f1e8c76d12d57ebd28edc4b
Massil Achab <massil.achab@gmail.com>
2024-12-20 22:05:59 +0100

    fix styling issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://tisura-labs.github.io',
+  base: '/landing-page',
   integrations: [tailwind()]
 });
