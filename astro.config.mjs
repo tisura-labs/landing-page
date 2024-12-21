7c86f44e5e1041fa0c95a52818f9a13161b2be5d
Massil Achab <massil.achab@gmail.com>
2024-12-21 20:05:26 +0100

    changed domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://tisura-labs.github.io',
-  base: '/landing-page',
+  site: 'https://tisura.xyz',
   integrations: [tailwind()]
 });
